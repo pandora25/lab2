9731a1d15b5d805d460bb35744016c9607fa1bd9
MHY Express <mcyalda@yahoo.com>
2018-04-25 11:34:33 -0400

    Lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp7_gc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            string input;
+            decimal area, Perimeter, x, y;
+
+
+            //bool user = true;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Welcome to Grand circus\' Detail Genrator ");
+            Console.Beep(120, 5000);
+            //process
+
+            while (true)
+            {
+                Console.ResetColor();
+                Console.Write("Enter Length ");
+                x = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Enter Width  ");
+                y = decimal.Parse(Console.ReadLine());
+
+                //process
+                area = x * y;
+                Perimeter = 2 * (x + y);
+
+                //output
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter:" + Perimeter);
+
+
+
+                Console.WriteLine("whould you like to continue (y/n)");
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "n")
+                {
+                    Console.WriteLine("See you later cuz you typn {0} ", input);
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
